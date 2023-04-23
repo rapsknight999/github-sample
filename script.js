08c25b99e745b53a52d398b1e9304a6c3cc3660b
Stevepurpose <stephenodogwu83@gmail.com>
2023-04-23 03:49:42 +0100

    added background colors

--- script.js
+++ script.js
@@ -7,19 +7,23 @@ let h1=document.querySelector("h1")
 
 if(pick==2){
     h1.style.color="blue"
+    h1.style.backgroundColor="yellow"
 }
 
 
 if(pick==4){
     h1.style.color="red"
+    h1.style.backgroundColor="blue"
 }
 
 if(pick==8){
     h1.style.color="orange"
+    h1.style.backgroundColor="green"
 }
 
 if(pick==11){
     h1.style.color="purple"
+    h1.style.backgroundColor="black"
 }
 
 
